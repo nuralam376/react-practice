9f40ea5938a28a5f6e46b0baacef2ba9671ddb3e
Nur A Alam <nuraalam376@gmail.com>
2021-03-24 08:58:29 +0600

    started learning react redux

---
+++ redux-basic/react-redux/src/components/CakeContainer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const CakeContainer = () => {
+  return (
+    <>
+      <h1>Num of cakes</h1>
+      <button>Buy Cake</button>
+    </>
+  );
+};
+
+export default CakeContainer;
