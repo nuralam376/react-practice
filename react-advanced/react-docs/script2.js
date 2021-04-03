2bfd32e7264b7bf5b53927f318c1d319d080b49a
Nur A Alam <nuraalam376@gmail.com>
2021-04-03 22:03:29 +0600

    practiced js virtual dom

--- react-advanced/react-docs/script2.js
+++ react-advanced/react-docs/script2.js
@@ -1,1 +1,11 @@
 const root = document.querySelector("#root");
+
+let number = " ";
+let i = 0;
+while (i <= 10000) {
+  number = number + i + " ";
+  i++;
+  root.innerHTML = number;
+}
+
+// root.appendChild(number);
