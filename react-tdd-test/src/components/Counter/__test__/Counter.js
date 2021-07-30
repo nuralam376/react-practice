faa9b6d542e62e0974f5b0ba6d70ca0f33f0ee29
Nur A Alam <nuraalam376@gmail.com>
2021-07-30 12:57:07 +0600

    added tdd

--- react-tdd-test/src/components/Counter/__test__/Counter.js
+++ react-tdd-test/src/components/Counter/__test__/Counter.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 function Counter() {
+    const[counter,setCounter] = useState(0);
+    const [inputValue,setInputValue] = useState(1);
+
     return (
         <div>
             <h1 data-testid = "header">Counter</h1>
+            <h2 data-testid = "counter">{counter}</h2>
+            <button data-testid = "subtract-btn">-</button>
+            <input type="number" data-testid = "input" value = {inputValue}/>
+            <button data-testid = "add-btn">+</button>
         </div>
     )
 }
--- react-tdd-test/src/components/Counter/__test__/__test__/Counter.test.js
+++ react-tdd-test/src/components/Counter/__test__/__test__/Counter.test.js
@@ -7,3 +7,27 @@ test("should render header text", () => {
     const headerElement = screen.getByTestId("header");
     expect(headerElement.textContent).toBe("Counter");
 });
+
+test("should show counter value 0", () => {
+    render(<Counter />);
+    const counterEl = screen.getByTestId("counter");
+    expect(counterEl.textContent).toBe("0");
+});
+
+test("should show input value 1", () => {
+    render(<Counter />);
+    const inputEl = screen.getByTestId("input");
+    expect(inputEl.value).toBe("1");
+});
+
+test("should show subtract button", () => {
+    render(<Counter />);
+    const subtractBtn = screen.getByTestId("subtract-btn");
+    expect(subtractBtn.textContent).toBe("-");
+});
+
+test("should show add btn", () => {
+    render(<Counter />);
+    const addBtn = screen.getByTestId("add-btn");
+    expect(addBtn.textContent).toBe("+");
+});
